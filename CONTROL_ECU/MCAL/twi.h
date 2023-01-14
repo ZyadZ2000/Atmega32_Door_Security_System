fdd3c7c3628033bfe93a31f20d3d4de19026c225
Zyad Nezar <85132955+ZyadZ2000@users.noreply.github.com>
2023-01-14 21:49:24 +0200

    Update twi.h

--- CONTROL_ECU/MCAL/twi.h
+++ CONTROL_ECU/MCAL/twi.h
@@ -6,7 +6,7 @@
  *
  * Description: Header file for the TWI(I2C) AVR driver
  *
- * Author: Mohamed Tarek
+ * Author: Zyad Nezar
  *
  *******************************************************************************/ 
 
